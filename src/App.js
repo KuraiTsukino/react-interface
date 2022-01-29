81f22bb8055e9c23112b7f1a9bacdf2cb5465903
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-29 04:04:21 -0600

    Searching with a filtered array

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import AppointmentInfo from "./components/AppointmentInfo";
 function App() {
 
   let [appointmentList, setAppointmentList] = useState([])
+  let [query, setQuery] = useState("")
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+        )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -25,17 +36,18 @@ function App() {
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top"/> Your Appointments</h1>
       <AddApointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)} />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList
+        {filteredAppointments
           .map(appointment => (
             <AppointmentInfo key={appointment.id} 
               appointment={appointment}
               onDeleteAppointment={
                 appointmentId => 
                   setAppointmentList(appointmentList.filter(appointment => 
-                  appointment.id !== appointmentId))
+                    appointment.id !== appointmentId))
               }
             />
         ))
