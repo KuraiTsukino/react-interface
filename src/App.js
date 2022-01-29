07fa02d850c30d5fc6ff1380ba532fefd380f8b8
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-29 02:39:38 -0600

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import { BiArchive } from "react-icons/bi"
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Your Appointments</h1>
+      <h1 className="text-3xl font-bold underline"><BiArchive /> Your Appointments</h1>
     </div>
   );
 }
